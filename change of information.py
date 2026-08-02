a8861d1f185fa35ab9555be382611faaea82649a
SHADOW-TDS52 <stepan.dv2020@gmail.com>
2026-08-02 20:37:19 +1000

    Delete change of information.py

--- change of information.py
+++
@@ -1,9 +0,0 @@
-import sqlite3
-def update_reminder(reminder_id: int, date: str, event: str, time: str) -> None:
-    with sqlite3.connect('reminder.db') as con:
-        con.execute(
-            "UPDATE reminder SET date = ?, event = ?, time = ? WHERE id = ?;",
-            (date, event, time, reminder_id),
-        )
-if __name__ == '__main__':
-    update_reminder(2, '2027-02-16', 'Встреча с клиентом', '14:00:00')
