9674e9ea2d9285c787017c4a5d26e5e249e2d79e
SHADOW-TDS52 <stepan.dv2020@gmail.com>
2026-08-02 20:37:10 +1000

    Delete adding information.py

--- adding information.py
+++
@@ -1,9 +0,0 @@
-import sqlite3
-def add_reminder(reminder_id: int, date: str, event: str, time: str) -> None:
-    with sqlite3.connect('reminder.db') as con:
-        con.execute(
-            "INSERT INTO reminder (id, date, event, time) VALUES (?, ?, ?, ?);",
-            (reminder_id, date, event, time),
-        )
-if __name__ == '__main__': 
-    add_reminder(2, '2027-02-15', 'Конференция', '09:00:00')
